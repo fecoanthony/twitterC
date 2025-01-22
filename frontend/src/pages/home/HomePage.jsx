01cbc31a984fed32d200618a993cbc2e22e9f76f
anthony <fecotony@gmail.com>
2025-01-22 04:32:45 -0800

    fetch post for the homepage

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -8,7 +8,6 @@ const HomePage = () => {
 
 	return (
 		<>
-
 			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
 				{/* Header */}
 				<div className='flex w-full border-b border-gray-700'>
@@ -38,7 +37,7 @@ const HomePage = () => {
 				<CreatePost />
 
 				{/* POSTS */}
-				<Posts />
+				<Posts feedType={feedType} />
 			</div>
 		</>
 	);
